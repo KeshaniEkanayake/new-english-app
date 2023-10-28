0249c15f115b34852ab2588ac96715357f51a757
KeshaniEkanayake <dimagikeshani5@gmail.com>
2023-10-29 00:21:18 +0530

    select module page is partially done

---
+++ src/components/AvatarOne.vue
@@ -0,0 +1,24 @@
+<template>
+    <div >
+      <img src="../assets/images/avatars/avatarOne.png" alt="AvatarOne" class="avatar-container">
+      
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style scoped>
+  .avatar-container {
+    position: absolute;
+    top: 20%;
+    right: 2%; /* Adjust the left position as needed */
+    /* Add any styles for the text container here */
+    width: 30%;
+  
+  }
+  
+  </style>
---
+++ src/views/SelectModule.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="background1">
+    <SideNavigation/>
+
+    <CommentShape>
+     <!-- Add dynamic text content here with a line break -->
+     <div v-html="dynamicText"></div>
+    </CommentShape>
+
+    <div class="outer-container">
+      
+      <AvatarOne/>
+    </div>
+    
+  </div>
+</template>
+
+
+<script>
+    import { defineComponent } from "vue"
+    import CommentShape from "@/components/CommentShape.vue"
+    import AvatarOne from "@/components/AvatarOne.vue"
+    import SideNavigation from "@/components/SideNavigation.vue"
+
+    import "../assets/css/main.css"
+
+    export default defineComponent({
+        components:{
+          SideNavigation, CommentShape, AvatarOne
+            },
+
+            data() {
+              return {
+                dynamicText: "Select the module <br> you want to learn",
+              };
+            },
+        })
+
+</script>
+
+<style scoped>
+
+  .outer-container{
+    display: flex;
+    justify-content: space-between; /* Align the content and AvatarOne component to the right */
+
+  }
+</style>
