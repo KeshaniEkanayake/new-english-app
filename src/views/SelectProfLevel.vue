866f6455db49c35924c511d55f0097865da22319
KeshaniEkanayake <dimagikeshani5@gmail.com>
2023-11-01 13:36:53 +0530

    added links to the select prof level button-images

--- src/views/SelectProfLevel.vue
+++ src/views/SelectProfLevel.vue
@@ -2,11 +2,19 @@
     <div class="background1">
             <SideNavigation/> 
             
-            <img src="../assets/images/basicProfButton.png" alt="basic prof level button" class="basicProfButton">
-            <img src="../assets/images/intermediateProfButton.png" alt="basic prof level button" class="intermediateProfButton">
-            <img src="../assets/images/advancedProfButton.png" alt="basic prof level button" class="advancedProfButton">
+            <a href="/basic-prof-url" class="image-link">
+                <img src="../assets/images/basicProfButton.png" alt="basic prof level button" class="basicProfButton">
+            </a>
 
-        
+            <a href="/basic-prof-url" class="image-link">
+                <img src="../assets/images/intermediateProfButton.png" alt="basic prof level button" class="intermediateProfButton">
+            </a>
+
+            <a href="/basic-prof-url" class="image-link">
+                <img src="../assets/images/advancedProfButton.png" alt="basic prof level button" class="advancedProfButton">   
+            </a>
+
+            
     </div>
         
 
@@ -16,9 +24,11 @@
 import { defineComponent } from "vue"
 import SideNavigation from "@/components/SideNavigation.vue"
 
+
     export default defineComponent({
         components:{
-            SideNavigation
+            SideNavigation,
+            
         }
     })
 </script>
@@ -41,26 +51,24 @@ import SideNavigation from "@/components/SideNavigation.vue"
         position: absolute; /* Position the image absolutely */
         top: 40%; /* Adjust the top distance as needed */
         right: 45%; /* Adjust the right distance as needed */
-        width: 25%;
-       
-        
+        width: 25%;       
     }
 
     .intermediateProfButton{
         position: absolute; /* Position the image absolutely */
         top: 60%; /* Adjust the top distance as needed */
         right: 45%; /* Adjust the right distance as needed */
-        width: 25%;
-       
-        
+        width: 25%;        
     }
 
     .advancedProfButton{
         position: absolute; /* Position the image absolutely */
         top: 80%; /* Adjust the top distance as needed */
         right: 45%; /* Adjust the right distance as needed */
-        width: 25%;
-       
-        
+        width: 25%;     
     }
+
+
+
+
 </style>
