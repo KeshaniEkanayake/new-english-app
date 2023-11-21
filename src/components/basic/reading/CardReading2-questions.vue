01825745e4e3a67547bfc0d974e9b9b0a056d568
saiNan24 <saitharsan@gmail.com>
2023-11-21 11:45:44 +0530

    Update CardReading2-questions.vue

--- src/components/basic/reading/CardReading2-questions.vue
+++ src/components/basic/reading/CardReading2-questions.vue
@@ -1,5 +1,24 @@
 <!-- has a drag and drop functionality -->
 <template>
+
+   <!-- for correct answers -->
+   <div v-if="correctAnswer" @click="handleCorrectAnswer" class="blur-background"></div>
+   <div v-if="correctAnswer" class="task-avatar">
+     <img src="../../../assets/images/avatars/avatarOne.png" alt="" />
+   </div>
+   <div v-if="correctAnswer" class="task-text-box">
+     Congratulations !!! you have successfully completed the task
+   </div>
+ 
+   <!-- for wrong answers -->
+   <div v-if="wrongAnswer" @click="handleWrongAnswer" class="blur-background"></div>
+   <div v-if="wrongAnswer" class="task-avatar">
+     <img src="../../../assets/images/avatars/avatarOne.png" alt="" />
+   </div>
+   <div v-if="wrongAnswer" class="task-text-box">
+     Opps !!! that's wrong. Take your time and think a little.
+   </div>
+
   <n-card>
     <h3>Match the phrases with the sentences. Drag the correct answer and drop it near the question.</h3>
 
@@ -39,7 +58,7 @@
     <br>
 
     <div class="center-item">
-      <ButtonCheck @click="checkAnswers" />
+      <ButtonCheck @click="handleAnswerCheck"/>
     </div>
   </n-card>
 </template>
@@ -54,7 +73,52 @@ export default {
     ButtonAnswer,
     ButtonCheck,
   },
+
+  data(){
+    return{
+      correctAnswer: false,
+      wrongAnswer: false,
+    }
+  },
+
+  methods:{
+    handleWrongAnswer() {
+      this.wrongAnswer = !this.wrongAnswer;
+    },
+
+    handleCorrectAnswer() {
+      this.correctAnswer = !this.correctAnswer;
+    },
+
+    handleAnswerCheck() {
+      var flag = 1;
+      // console.log(this.checkAnswers);
+      if(this.checkAnswers.value != 0){
+        console.log(this.checkAnswers);
+        for( const value in this.checkAnswers.value){
+          
+          if(this.checkAnswers.value[value].isCorrect === false){
+            // console.log(this.checkAnswers.value[value].isCorrect);
+            this.handleWrongAnswer();
+            flag = 0;
+            break;
+          }
+        }
+
+        if(flag === 1){
+          this.handleCorrectAnswer();
+        }
+        
+      }else{
+        this.handleWrongAnswer();
+      }
+    }
+  },
+
   setup() {
+
+    const checkAnswers = {value:0};
+
     const answers = ref([
       { id: 1, text: "Final call" },
       { id: 2, text: "Cancelled" },
@@ -65,14 +129,14 @@ export default {
     ]);
 
     const questions = ref([
-      { id: 1, text: "1. The plane isn't here now." },
-      { id: 2, text: "2. The plane is late." },
-      { id: 3, text: "3. The plane is not going to leave." },
-      { id: 4, text: "4. You can get on the plane now." },
-      { id: 5, text: "5. The plane is leaving very soon." },
-      { id: 6, text: "6. You need to go to a different place to get on the plane." },
+      { id: 1, text: "1. The plane isn't here now.", answer: "Departed"},
+      { id: 2, text: "2. The plane is late.", answer: "Delayed"},
+      { id: 3, text: "3. The plane is not going to leave.", answer: "Cancelled" },
+      { id: 4, text: "4. You can get on the plane now.", answer: "Boarding" },
+      { id: 5, text: "5. The plane is leaving very soon.", answer: "Final call" },
+      { id: 6, text: "6. You need to go to a different place to get on the plane.", answer: "Gate change" },
     ]);
-
+    
     const selectedAnswers = ref({});
     const isDragOver = ref({});
 
@@ -89,14 +153,23 @@ export default {
     };
 
     const drop = (questionId) => {
-      const answerText = event.dataTransfer.getData("text/plain");
-      selectedAnswers.value = { ...selectedAnswers.value, [questionId]: answerText };
-      isDragOver.value = { ...isDragOver.value, [questionId]: false };
-    };
 
-    const checkAnswers = () => {
-      // Implement your logic to check the correctness of answers
-      console.log(selectedAnswers.value);
+        const answerText = event.dataTransfer.getData("text/plain");
+
+        // Update checkAnswers with the question ID and dropped answer
+        checkAnswers.value = {
+          ...checkAnswers.value,
+          [questionId]: {
+            answer: answerText,
+            isCorrect: answerText === questions.value.find(q => q.id === questionId)?.answer
+          },
+        };
+
+        // Update selectedAnswers and isDragOver as before
+        selectedAnswers.value = { ...selectedAnswers.value, [questionId]: answerText };
+        isDragOver.value = { ...isDragOver.value, [questionId]: false };
+        console.log(checkAnswers);
+
     };
 
     return {
@@ -148,5 +221,37 @@ export default {
 
 .center-item {
   text-align: center;
+}
+
+.blur-background {
+  background-color: black;
+  opacity: 0.7;
+  position: absolute;
+  width: 96%;
+  height: 100%;
+  z-index: 998;
+}
+
+.task-avatar {
+  position: absolute;
+  z-index: 999;
+  right: 0;
+  bottom: 0;
+}
+
+.task-text-box {
+  width: 500px;
+  height: auto;
+  background-color: #0c4f80;
+  border: #ffffff 2px solid;
+  z-index: 1000;
+  top: 10%;
+  position: absolute;
+  border-radius: 25px;
+  padding: 20px;
+  font-size: 22px;
+  text-align: center;
+  word-wrap: break-word;
+  color: #ffffff;
 }
 </style>
